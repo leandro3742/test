a38545bcf1c234acc13e00946a1a2466f301db5d
FacundoBauza <31938804+FacundoBauza@users.noreply.github.com>
2023-09-08 19:06:28 -0300

    Estructura_Commit-1

---
+++ PUB_Puerta Verde/DataAccesLayer/DataContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer
+{
+    public class DataContext: IdentityDbContext<Microsoft.AspNetCore.Identity.IdentityUser>
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext()
+        {
+        }
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to mysql with connection string from app settings
+            //var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+            //options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            //options.UseMySql("server=localhost; Port=1010; database=webapi; user=webapi; password=webapi", 
+            //                 ServerVersion.AutoDetect("server=localhost; Port=1010; database=webapi; user=webapi; password=webapi"));
+            //options.UseMySql("server=db-ria2022; database=webapi; user=webapi; password=webapi",
+            //     ServerVersion.AutoDetect("server=db-ria2022; database=webapi; user=webapi; password=webapi"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        //Ejemplo
+        /*public DbSet<Ejemplo> Noticias { get; set; }*/
+    }
+}

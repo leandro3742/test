83d8b4fd3926c7848c23133e0879839bb316568f
FacundoBauza <31938804+FacundoBauza@users.noreply.github.com>
2023-09-11 20:17:46 -0300

    Gitignore y Program.cs

--- PUB_Puerta Verde/WebApi-PUB_PV/Program.cs
+++ PUB_Puerta Verde/WebApi-PUB_PV/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "Web API - V1",
+            Version = "v1"
+        }
+     );
+});
 
 var app = builder.Build();
 
